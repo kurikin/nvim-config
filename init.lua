e99c220ea23793254ffa9a213f38da5afa960261
kurikin <riku.code0309@gmail.com>
2024-03-03 23:07:09 +0900

    Add settings for lazy.nvim

--- init.lua
+++ init.lua
@@ -15,4 +15,15 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins", {
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "netrw",
+        "netrwPlugin",
+        "netrwSettings",
+        "netrwFileHandlers",
+      },
+    },
+  },
+})
