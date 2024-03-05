0d356535bb133ed2254c7cf9e0e6fe02b88227c5
kurikin <riku.code0309@gmail.com>
2024-03-06 00:13:54 +0900

    Update config for lazyvim

--- init.lua
+++ init.lua
@@ -1,29 +1,5 @@
 require ('base')
 require ('options')
 require ('keymaps')
+require ('lazyvim')
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins", {
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "netrw",
-        "netrwPlugin",
-        "netrwSettings",
-        "netrwFileHandlers",
-      },
-    },
-  },
-})
