a350cac50314978e795dbd1ae607b948c5280a38
kurikin <riku.code0309@gmail.com>
2024-05-01 18:00:32 +0900

    Change color of line number

--- lua/theme.lua
+++ lua/theme.lua
@@ -3,4 +3,11 @@ require("tokyonight").setup({
   style = "moon",
 })
 
+function LineNumberColors()
+    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#737aa2' })
+    vim.api.nvim_set_hl(0, 'CursorLineNr', { fg='#ff9e64', bold=true })
+    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#737aa2' })
+end
+
 vim.cmd.colorscheme "tokyonight"
+LineNumberColors()
